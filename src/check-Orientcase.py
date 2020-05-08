ff6142e8540b2846ef646958819b0aff957a3ca2
cclin <181250083@smail.nju.edu.cn>
2020-05-08 16:43:40 +0800

    cc-update-Standard1

--- src/check-Orientcase.py
+++ src/check-Orientcase.py
@@ -15,7 +15,12 @@ false: 第line行目标代码不包含 print() 函数
 
 def standard1(line):
     standard1 = "print"
-    return line.find(standard1) != -1
+    if line.find("#") != -1:
+        return "NULL"
+    if(line.find(standard1) != -1):
+        return line
+    else:
+        return "NULL"
 
 
 '''
@@ -31,11 +36,29 @@ false: 断言非面向用例
 
 
 def standard1Judge(standard1Recorder):
+    illegalPrintFreq = 0
     printFreq = 0
     for i in range(standard1Recorder.__len__()):
-        if standard1Recorder[i]:
+        if standard1Recorder[i] == "NULL":
+            continue
+        else:
             printFreq += 1
-    return printFreq != 1
+            lineList = list(standard1Recorder[i])
+            start = standard1Recorder[i].find("(")
+            end = standard1Recorder[i].find(")")
+            # 撷取 print函数 '()' 之间的打印信息
+            printedContextlist = lineList[start+1: end]
+            printedContext = ''.join(printedContextlist)
+            if(printedContext.isnumeric()):
+                illegalPrintFreq += 1
+
+    if printFreq > 2:
+        return True
+    else:
+        if illegalPrintFreq != 0:
+            return True
+        else:
+            return False
 
 
 '''
@@ -93,9 +116,12 @@ false: 断言非面向用例
 
 def standard2Judge(standard2Recorder):
     syntaxJudge = [0 for _ in range(syntax.__len__())]
-    for i in range(standard2Recorder.__len__()):
-        for j in range(syntax.__len__()):
-            syntaxJudge[j] += standard2Recorder[i][j]
+    for i in range(syntaxJudge.__len__()):
+        for j in range(standard2Recorder.__len__()):
+            syntaxJudge[i] += standard2Recorder[j][i]
+        # print(syntax[i], end=" ")
+        # print(syntaxJudge[i])
+    # print(syntaxJudge)
     syntaxKinds = 0
     for i in range(syntaxJudge.__len__()):
         if syntaxJudge[i] == 0:
@@ -147,19 +173,18 @@ false: 断言非面向用例
 def checkOrientCase(targetCodepath):
     # 起初都先假设目标代码为非面向用例
     assertCodeIsOrientCase = False
-    with open(targetCodepath, "r") as f:
+    with open(targetCodepath, "r", encoding="utf-8") as f:
         standard1Recorder = []
         standard2Recorder = []
-        # 目标代码第一行
-        firstLine = f.readline()
         # 目标代码每一行，将存储为 list[] 的数据结构
         data = f.readlines()
+        # 目标代码第一行
+        firstLine = data[0]
+
         for i in range(data.__len__()):
             standard1Recorder.append(standard1(data[i]))
             standard2Recorder.append(standard2(data[i]))
-        print(standard1Judge(standard1Recorder))
-        print(standard2Judge(standard2Recorder))
-        print(standard3Judge(firstLine))
+
         assertCodeIsOrientCase = standard1Judge(standard1Recorder) and \
                                  standard2Judge(standard2Recorder) and \
                                  standard3Judge(firstLine)
@@ -169,7 +194,7 @@ def checkOrientCase(targetCodepath):
 
 
 
-
+'''
 #本地测试main函数
 
 if __name__ == '__main__':
@@ -177,17 +202,20 @@ if __name__ == '__main__':
     with open("D:\數據科學基礎/best-partner/test/test.py", "r", encoding="utf-8") as f:
         standard1Recorder = []
         standard2Recorder = []
-        firstLine = f.readline()
         data = f.readlines()
+        firstLine = data[0]
+        #print(data)
+        #print(firstLine)
         for i in range(data.__len__()):
             standard1Recorder.append(standard1(data[i]))
             standard2Recorder.append(standard2(data[i]))
-        print(standard1Judge(standard1Recorder))
-        print(standard2Judge(standard2Recorder))
-        print(standard3Judge(firstLine))
+        # print(standard1Recorder)
+        # print(standard2Recorder)
+        # print(standard1Judge(standard1Recorder))
+        # print(standard2Judge(standard2Recorder))
+        # print(standard3Judge(firstLine))
         assertCodeIsOrientCase = standard1Judge(standard1Recorder) and \
                                  standard2Judge(standard2Recorder) and \
                                  standard3Judge(firstLine)
         print(assertCodeIsOrientCase)
-
-
+'''
