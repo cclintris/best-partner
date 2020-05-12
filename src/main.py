21b747d2ee77c707c3586f93ccd90d3994f5ff6c
w2000703 <w2000703@hotmail.com>
2020-05-12 11:01:19 +0800

    add main optimize fetch_data

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import fetch_Data
+import grouping_And_Statistics
+
+if __name__ == "__main__":
+    filepath = "../sample.json"
+    Data = fetch_Data.Data(filepath)
+    # group = grouping_And_Statistics.group_by()
+
+
