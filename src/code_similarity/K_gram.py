3fa410a54944bb9937788ffee3dfc2b4dacc078e
cclin <181250083@smail.nju.edu.cn>
2020-07-05 14:25:16 +0800

    change file name to code_similarity

--- src/code_similarity/K_gram.py
+++ src/code_similarity/K_gram.py
@@ -8,6 +8,7 @@ def generateKgram(file, k):
         kgram.append(shingle)
     return kgram
 
+'''
 k = 3
 if __name__ == '__main__':
     with open("D:\\數據科學基礎/best-partner/test/test.txt", "r", encoding="utf-8") as f:
@@ -16,5 +17,4 @@ if __name__ == '__main__':
         file = data[0]
         kgram = generateKgram(file, k)
         print(kgram)
-
-
+'''
