96dec0e64c77204d79b805a7a7376253546af7bb
181250119 <982674309@qq.com>
2020-07-05 17:00:57 +0800

    feat: 复杂度/更新了对循环逻辑的处理

--- src/code_complexity/time_complexity.py
+++ src/code_complexity/time_complexity.py
@@ -30,7 +30,7 @@ class TimeChecker:
         1. 该方法将忽略任何引自其他文件的方法复杂度，将其视为O(1);
         2. 该方法将传入参数产生的for循环均视为O(n)复杂度，非参数产生的for循环均视为O(m)复杂度，原则上m<<n;
         3. 该方法将传入参数产生且不含有整除条件的while循环均视为O(n)复杂度，非参数产生的while循环均视为O(m)复杂度，
-           带有二分条件的分别视为O(log_2n)和O(log_2m)复杂度
+           带有二分条件的分别视为O(log_n)和O(log_m)复杂度
         :param method: 检查方法的名字
         :param method_begin: 方法的起始位置
         :param method_end: 方法的结束位置
@@ -40,42 +40,88 @@ class TimeChecker:
         if method in self.methods_complexity:
             return self.methods_complexity[method]
 
-        reserved_words = ["for", "while", "do"]
+        # 创建保留字列表，复杂度标记;提取参数名
+        reserved_words = ["for", "while"]
         complexity_tag = ['' for i in range(len(self))]
+        params = re.search("[(][^()]*[)]", self.codes[method_begin]).group()[1:-1].split(',')
+        for i in range(len(params)):
+            params[i] = re.match("[^:]*", params[i]).group()
 
-        def deal_loop(loop_type: str):
+        def deal_loop(loop_type: str, loop_index: int):
             """
             根据缩进树在缩进层级上标记出独立复杂度
             :param loop_type: 循环关键字的种类
+            :param loop_index: 循环关键字的所在行号
             :return:
             """
+            if loop_type == "for":
+                for param in params:
+                    if re.search(param, self.codes[loop_index]):
+                        complexity_tag[loop_index] = 'n'
+                if complexity_tag[loop_index] == '':
+                    complexity_tag[loop_index] = 'm'
+                return
+            elif loop_type == "while":
+                for param in params:
+                    if re.search(param, self.codes[loop_index]):
+                        if self.codes[loop_index].count('/'):
+                            complexity_tag[loop_index] = 'log_n'
+                        else:
+                            complexity_tag[loop_index] = 'n'
+                if complexity_tag[loop_index] == '':
+                    if self.codes[loop_index].count('/'):
+                        complexity_tag[loop_index] = 'log_m'
+                    else:
+                        complexity_tag[loop_index] = 'm'
+                return
             return
 
         def deal_recursion(code: str):
             """
-            根据递归代码计算递归复杂度
+            根据递归代码计算递归复杂度，该方法有以下限制：
+            1. 仅仅以常见的递归复杂度为模板估算复杂度，而不进行详尽的变量扫描和计算得出结论;
+            2. 任意自递归调用的参数均被视为n级别;
+            3. 常见的自递归函数复杂度由下列出：
+                T(n)=T(n/2)+O(1)         T(n)=O(log_n)
+                T(n)=T(n-1)+O(1)         T(n)=O(n)
+                T(n)=2*T(n/2)+O(1)       T(n)=O(n)
+                T(n)=2*T(n/2)+O(n)       T(n)=O(n*log_n)
+                T(n)=2*T(n/2)+O(n*log_n) T(n)=O(n*log^2_n)
+                T(n)=T(n-1)+O(n)         T(n)=O(n^2)
+                T(n)=2*T(n-1)+O(1)       T(n)=O(2^n)
+                T(n)=T(n-1)+T(n-2)+O(1)  T(n)=O(2^n)
             :param code: 产生递归的代码
             :return:
             """
+            if code.count('='):
+                code = code.split('=')[1]
+            has_rec = re.search(method, code)
+            while has_rec:
+                # 代码中仍存在自递归调用时
+                rec_start = has_rec.start()
+                rec_params = re.search("[^()]*", code[has_rec.end() + 1:])
+                rec_end = rec_params.end() + 1
+                rec_params = rec_params.group().split(',')
+                for rec_param in rec_params:
+                    if not rec_param.lstrip('-').isdigit():
+                        # 作为非数字的参数，检查减号和除号进行匹配
+                        return
 
         def integration_complexity() -> str:
             """
             整合complexity_tag上所记录的单行产生的复杂度，并返回整体复杂度
             :return: 方法的时间复杂度
             """
+            return ''
 
-        # 提取参数名
-        params = re.search("([^()]*)", self.codes[method_begin]).group()[1:-1].split(',')
-        for i in range(len(params)):
-            params[i] = re.match("[^:]*", params[i]).group()
         for i in range(method_begin + 1, method_end):
             method_line = self.codes[i].lstrip()
             # 扫描行首是否有循环保留字
             for word in reserved_words:
                 is_match = re.match(word, method_line)
                 if is_match:
-                    deal_loop(word)
-            # 扫描是否是递归方法
+                    deal_loop(word, i)
+            # 扫描是否是自递归方法
             is_recursion = re.search(method, method_line)
             if is_recursion:
                 deal_recursion(method_line)
