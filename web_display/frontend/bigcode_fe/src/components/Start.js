1d1cc649d1de4e94b171663efc55a866d2ede5ae
cclin <181250083@smail.nju.edu.cn>
2020-07-09 20:17:14 +0800

    update frontend web pages

--- web_display/frontend/bigcode_fe/src/components/Start.js
+++ web_display/frontend/bigcode_fe/src/components/Start.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.css'
 import { Input } from 'antd'
+import { Button } from 'antd'
+import { Divider } from 'antd'
+import { CheckOutlined } from '@ant-design/icons'
+import { Descriptions } from 'antd'
 
 export class Start extends Component {
+    confirm = () => {
+        console.log("confirm student id")
+    }
+
     render() {
         return (
             <div className="startlayout">
-                <Input placeholder="请输入学生 id :" id="inputStudentid"></Input>
+                <Input placeholder="请输入学生 id :" id="inputid"></Input>
+                <Divider type="vertical"/>
+                <Input placeholder="请输入题目 id :" id="inputid"></Input>
+                <Divider type="vertical"/>
+                <Button type="primary" 
+                        icon={<CheckOutlined />} 
+                        onClick={() => this.confirm()}>
+                        确认
+                </Button>
+                <Divider />
+                <Descriptions title="学生编程评价报告" bordered>
+                    <Descriptions.Item label="学生姓名" span={1}>林希澄</Descriptions.Item>
+                    <Descriptions.Item label="学生学号" span={1}>181250083</Descriptions.Item>
+                    <Descriptions.Item label="代码相似度" span={1}>25%</Descriptions.Item>
+                    <Descriptions.Item label="代码时空复杂度" span={1}>O(n)</Descriptions.Item>
+                    <Descriptions.Item label="整体评价">菜鸡一个</Descriptions.Item>
+                </Descriptions>
             </div>
         )
     }
--- web_display/frontend/bigcode_fe/src/views/main.js
+++ web_display/frontend/bigcode_fe/src/views/main.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import 'antd/dist/antd.css';
-import { Start } from '../components/Start';
-import { Tabs } from 'antd';
+import React, { Component } from 'react'
+import 'antd/dist/antd.css'
+import { Start } from '../components/Start'
+import { Tabs, Divider } from 'antd'
+import { DingtalkOutlined } from '@ant-design/icons'
+import { CopyrightCircleOutlined } from '@ant-design/icons'
 
-const { TabPane } = Tabs;
+const { TabPane } = Tabs
 
 export class main extends Component {
     render() {
         return (
             <div className="layout">
                 <div className="header">
-                    BigCode Tool
+                    <DingtalkOutlined></DingtalkOutlined>
+                    <span className="header-text">Code Evaluation Tool</span>
                 </div>
                 <div className="main">
                     <Tabs defaultActiveKey="1" size="large">
                         <TabPane tab="工具介绍" key="1">
-                            Content of Tab Pane 1
+                            README of tool usage
                         </TabPane>
                         <TabPane tab="开始使用" key="2">
                             <Start />
@@ -23,7 +26,10 @@ export class main extends Component {
                     </Tabs>
                 </div>
                 <div className="footer">
-
+                    <span className="footer-text">
+                        数据科学基础大作业 / 由 BigCode 小组开发
+                        <CopyrightCircleOutlined />
+                    </span>
                 </div>
             </div>
         )
