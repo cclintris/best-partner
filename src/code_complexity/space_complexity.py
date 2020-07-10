895a6444e5b9f3af62c4de73d7ab19529cd14893
181250119 <982674309@qq.com>
2020-07-10 23:05:56 +0800

    feat: 复杂度/提交了空间复杂度的计算，基本复用了time_complexity的思路并稍作了一些改动

--- src/code_complexity/space_complexity.py
+++ src/code_complexity/space_complexity.py
@@ -15,12 +15,31 @@ class SpaceChecker(Checker):
         1. 任意无循环添加列表的变量均被视为O(1)，包括递归
         2. 含有任意循环添加的空间复杂度被视为O(n^x)
         3. 忽略任何可能出现的条件筛选/二分法导致的添加并视为O(1)
-        :param method:
-        :param method_begin:
-        :param method_end:
+        4. 对出现的递归调用，将该方法的复杂度增加O(n)
+        :param method: 方法名
+        :param method_begin: 方法起始位置
+        :param method_end: 方法结束位置
         :return:
         """
-        return 0
+        if method in self.methods_complexity:
+            return self.methods_complexity[method]
+        deal_recursion = False
+        for i in range(method_begin + 1, method_end):
+            method_line = self.codes[i].lstrip()
+            # 扫描行首是否有循环保留字
+            for word in self.reserved_words:
+                is_match = re.match(word, method_line)
+                if is_match:
+                    self.deal_loop(i, self.codes, self.indentation_structure, self.complexity_tag)
+            # 扫描是否是自递归方法
+            is_recursion = re.search(method, method_line)
+            if is_recursion:
+                deal_recursion = True
+        res = self.integrate_complexity(method_begin, method_end, self.indentation_structure, self.complexity_tag)
+        if deal_recursion:
+            return res * CompStr("n")
+        else:
+            return res
 
     def cal_main_complexity(self, main_codes):
         """
@@ -28,10 +47,26 @@ class SpaceChecker(Checker):
         1. 以input()函数为基准，将输入的数据作为列表项储存/作为列表长度声明的时候复杂度为O(n)
         2. 输入数据不做储存仅做声明时，将复杂度视为O(1)
         3. 最终的空间复杂度为所有的列表项、字典项储存的最大数据量
+        TODO 该方法复用了与time_complexity完全相同的逻辑处理，有待重构
         :param main_codes:
         :return:
         """
-        return 0
+        main_indentation = []
+        for i in range(len(main_codes)):
+            main_indentation.append((len(main_codes[i]) - len(main_codes[i].lstrip())) // 4)
+        main_comp_tag = [CompStr('1') for i in range(len(main_codes))]
+        methods_call_index = self.cal_method_call_index(main_codes)
+        for k, v in methods_call_index.items():
+            for q in v:
+                main_comp_tag[q] = self.methods_complexity[k]
+        for i in range(len(main_codes)):
+            line = main_codes[i].lstrip()
+            # 扫描行首是否有循环保留字
+            for word in self.reserved_words:
+                is_match = re.match(word, line) is not None or re.search(" " + word + " ", line) is not None
+                if is_match:
+                    self.deal_loop(i, main_codes, main_indentation, main_comp_tag)
+        return self.integrate_complexity(-1, len(main_codes), main_indentation, main_comp_tag)
 
     def deal_loop(self, loop_index: int, codes: list, indentation_structure: list, complexity_tag: list):
         """
@@ -55,7 +90,7 @@ class SpaceChecker(Checker):
                 complexity_tag[loop_index] = CompStr("1")
                 return 0
 
-    def integrate_complexity(self, begin: int, end: int, indentation_structure: list, complexity_tag: list):
+    def integrate_complexity(self, begin: int, end: int, indentation_structure: list, complexity_tag: list) -> CompStr:
         """
         整合complexity_tag上的复杂度标签
         :param begin:
@@ -64,7 +99,7 @@ class SpaceChecker(Checker):
         :param complexity_tag:
         :return:
         """
-        return 0
+        return CompStr("1")
 
     def param_comp(self, code_line: str):
         """
@@ -75,7 +110,8 @@ class SpaceChecker(Checker):
         :param code_line: 一行代码
         :return:
         """
-        return re.match("[^\\\\.]*[\\\\.]append", code_line) or re.match("[^[]*[[][^=]*=", code_line)
+        return re.match("[^\\\\.]*[\\\\.]append", code_line) is not None or re.match("[^[]*[[][^=]*=",
+                                                                                     code_line) is not None
 
 
 if __name__ == '__main__':
